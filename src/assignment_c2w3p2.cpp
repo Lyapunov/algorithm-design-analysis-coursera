d167ac875f339a7d274dd20a6f369c3effe4640e
Lyapunov <vaskorichard@gmail.com>
2016-07-04 06:47:08 +0200

    Refactoring, but over around 7.0s.

--- src/assignment_c2w3p2.cpp
+++ src/assignment_c2w3p2.cpp
@@ -20,13 +20,7 @@ unsigned long tabletKey( int i, int W ) {
 }
 
 int knapsack( int i, int W, const std::vector< Item >& items, std::unordered_map<unsigned long, int>& tablet ) {
-   // trivial cases
-   if ( i <= 0 ) {
-      return 0;
-   }
-
-   const Item& myItem = items[i - 1];
-
+   const Item& myItem = items[i];
    if ( W < myItem.weight ) {
       return 0;
    }
@@ -58,6 +52,12 @@ int knapsack( int i, int W, std::vector< Item >& items ) {
       return a.weight >= b.weight;
    });
 
+   // fake item to simplify zero check
+   Item fake;
+   fake.weight = INT_MAX;
+   fake.value  = 0;
+   items.insert( items.begin(), fake );
+
    if ( DEBUG_MODE ) {
       int total = 0;
       std::cout << "--- i = " << items.size() << ", W = " << W << std::endl;
@@ -87,6 +87,7 @@ int main( int argc, const char* argv[] ) {
       std::string filename = argv[1];
       int knapsack_size = 0;
       std::vector< Item > items;
+      items.reserve(4000);
       if ( !readInput( argv[1], knapsack_size, items, DEBUG_MODE ) ) {
          std::cerr << "ERROR during attempting to read file " << filename << std::endl;
          return 1;
