0053846ede25d37caa5093edebf2cf606803c1af
Lyapunov <vaskorichard@gmail.com>
2016-07-11 03:03:45 +0200

    Working on implementing Dijkstra's algorithm.

---
+++ src/assignment_c1w5p1.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <string.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <set>
+#include <vector>
+#include <algorithm>
+
+#include "graph_adjacency_list.h"
+
+static const int DEBUG_MODE = 1;
+
+int main( int argc, const char* argv[] ) {
+   // Prints each argument on the command line.
+   if ( argc < 1 ) {
+      return 1;
+   }
+
+   if ( argc != 2 ) {
+      std::cout << "USAGE: " << basename( argv[0] ) << " <filename> " << std::endl;
+      return 0;
+   } else{ 
+      GraphAL graph;
+      std::string filename = argv[1];
+      if ( !readGraph( filename, graph, DEBUG_MODE ) ) {
+         std::cerr << "ERROR during attempting to read file " << filename << std::endl;
+         return 1;
+      }
+
+      std::cout << graph << std::endl;
+   }
+}
---
+++ src/graph_adjacency_list.h
@@ -0,0 +1,92 @@
+#ifndef GRAPH_ADJACENCY_LIST
+#define GRAPH_ADJACENCY_LIST
+
+#include <cassert>
+
+struct Edge {
+   int first = -1;
+   int second = 1;
+   int cost = -1;
+
+   bool isValid() {
+      return first >= 0;
+   }
+};
+
+struct GraphAL {
+   unsigned n = 0;
+   std::vector< std::vector<Edge> > alist;
+};
+
+std::ostream& operator<<( std::ostream& os, const Edge& edge ) {
+   os << "Edge( " << edge.first << ", " << edge.second << ", " << edge.cost << ")";
+   return os;
+}
+
+std::ostream& operator<<( std::ostream& os, const GraphAL& graph ) {
+   os << "GraphAL( " << graph.n << std::endl;
+   for ( unsigned i = 0; i < graph.alist.size(); ++i ) {
+      os << "--- " << i << ": ";
+      for ( const auto& elem : graph.alist[i] ) {
+         os << elem.second << "[" << elem.cost << "] ";
+      }
+      os << std::endl;
+   }
+   os << ")";
+   return os;
+}
+
+static bool readGraph( std::string filename, GraphAL& graph, int debugmode = 0 )
+{
+   std::ifstream is;
+   is.open( filename.c_str() );
+
+   // first line
+   std::string line;
+   if ( !std::getline( is, line ) ) {
+      return false;
+   }
+   std::istringstream ss(line);
+
+   GraphAL retval;
+
+   if ( !(ss >> retval.n ) ) {
+      return false;
+   }
+
+   if ( debugmode ) {
+      std::cout << "=== READING GRAPH(" << retval.n << ")" << std::endl;
+   }
+
+   int number = 0;
+   int loopNumber = 0;
+   while (true) {
+      if ( !std::getline( is, line ) ) {
+         is.close();
+         graph = retval;
+         return true;
+      }
+      std::istringstream ss(line);
+      ++loopNumber;
+      ss >> number;
+      if ( loopNumber != number ) {
+         return false;
+      }
+      retval.alist.push_back( std::vector<Edge>() );
+      while ( ss ) {
+         Edge edge;
+         edge.first = number;
+         std::string trash;
+         ss >> edge.second;
+         std::getline(ss, trash, ',');
+         ss >> edge.cost;
+         if ( !ss ) {
+            continue;
+         }
+         std::cout << edge << std::endl;
+         retval.alist[number - 1].push_back( edge );
+      }
+   }
+}
+
+#endif /*GRAPH_ADJACENCY_LIST*/
