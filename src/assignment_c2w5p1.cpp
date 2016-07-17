f6fa9ea1788701f264a8b5608de8abd43879307c
Lyapunov <vaskorichard@gmail.com>
2016-07-17 03:25:16 +0200

    Calculating binomial coefficients, maybe useful for numbering the permutations.

--- src/assignment_c2w5p1.cpp
+++ src/assignment_c2w5p1.cpp
@@ -9,6 +9,22 @@
 
 static const int DEBUG_MODE = 1;
 
+std::vector<unsigned>
+binomials( unsigned n ) {
+   std::vector<unsigned> retval;
+   retval.push_back( 1 );
+   for ( unsigned i = 0; i < n; ++i ) {
+      retval.push_back( 0 );
+      for ( unsigned j = retval.size() - 1; j > 0; --j ) {
+         retval[j] = retval[j] + retval[j - 1];
+      }
+   }
+   return retval;
+}
+
+void solve_tsp( const EuclidianGraph& egraph ) {
+}
+
 int main( int argc, const char* argv[] ) {
    // Prints each argument on the command line.
    if ( argc < 1 ) {
@@ -25,8 +41,16 @@ int main( int argc, const char* argv[] ) {
          std::cerr << "ERROR during attempting to read file " << filename << std::endl;
          return 1;
       }
+      
+      if ( DEBUG_MODE ) {
+         std::vector<unsigned> bs = binomials(4);
+         for ( const auto& elem : bs ) {
+            std::cout << elem << " ";
+         }
+         std::cout << std::endl;
+         std::cout << egraph << std::endl;
+      }
 
-      std::cout << egraph << std::endl;
    }
    return 0;
 }
