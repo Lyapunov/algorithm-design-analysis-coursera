ec477ed9446122e4390d42b119cd17819e84f909
Lyapunov <vaskorichard@gmail.com>
2016-07-27 04:42:35 +0200

    Refactoring only.

--- src/graph_adjacency_list.h
+++ src/graph_adjacency_list.h
@@ -180,8 +180,8 @@ bool readGraph( std::string filename, GraphAL& graph, int debugmode = 0 )
    }
 }
 
-template < class T = GraphAL >
-bool readALGraphFromEdgeList( std::string filename, T& graph, int debugmode = 0 )
+template < class GraphRepresentant = GraphAL >
+bool readALGraphFromEdgeList( std::string filename, GraphRepresentant& graph, int debugmode = 0 )
 {
    std::ifstream is;
    is.open( filename.c_str() );
@@ -190,7 +190,7 @@ bool readALGraphFromEdgeList( std::string filename, T& graph, int debugmode = 0 
       std::cout << "=== READING GRAPH()" << std::endl;
    }
 
-   T retval;
+   GraphRepresentant retval;
    std::string line;
    while (true) {
       if ( !std::getline( is, line ) ) {
